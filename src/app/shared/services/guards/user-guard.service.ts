34300a233d1cf99b3ae327f66a344c0542c254f1
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-03-12 11:53:00 -0300

    update to angular 16

--- src/app/shared/services/guards/user-guard.service.ts
+++ src/app/shared/services/guards/user-guard.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { TokenService } from '@app/shared/services/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuardService implements CanActivate {
+export class UserGuardService  {
 
   realRol: string;
 
